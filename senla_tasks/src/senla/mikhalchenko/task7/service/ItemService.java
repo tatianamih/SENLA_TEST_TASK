9430a4fa97fee4a33caa42561ed4a7854be9900f
“tatianamih” <tmihalchenko66@gmail.com>
2021-07-15 23:19:20 +0300

    Task7 Service to put items into the safe

---
+++ senla_tasks/src/senla/mikhalchenko/task7/service/ItemService.java
@@ -0,0 +1,73 @@
+package senla.mikhalchenko.task7.service;
+
+import senla.mikhalchenko.task7.bean.Item;
+import senla.mikhalchenko.task7.bean.Safe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static senla.mikhalchenko.task7.util.ItemDatabase.createItemDatabase;
+
+public class ItemService {
+    private final Safe safe;
+    private final List<Item> items;
+    private final Scanner scanner;
+
+    {
+        safe = new Safe();
+        items = createItemDatabase();
+        scanner = new Scanner(System.in);
+    }
+
+    public void printItemList() {
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public List<Item> sortByPrice() {
+        return items.stream()
+                .sorted((o1, o2) -> o2.getPrice() - o1.getPrice())
+                .collect(Collectors.toList());
+    }
+
+    public void printSortedListByPrice(List<Item> sortedListByPrice) {
+        sortedListByPrice.forEach(System.out::println);
+    }
+
+    public int findMinVolumeFromItemList() {
+        return items.stream()
+                .sorted((o1, o2) -> o1.getVolume() - o2.getVolume())
+                .collect(Collectors.toList()).get(0).getVolume();
+    }
+
+    public List<Item> putItemInTheSafeWithMaxPrice(List<Item> sortedList, int volume) {
+        List<Item> resultList = new ArrayList<>();
+        //   printNumberException();
+        safe.setVolume(volume);
+
+        for (int i = 0; i < sortedList.size(); i++) {
+            int sumVolume = sortedList.get(i).getVolume() * sortedList.get(i).getNumber();
+            if (safe.getVolume() >= sumVolume) {
+                resultList.add(new Item(sortedList.get(i).getName(), sortedList.get(i).getNumber(),
+                        sortedList.get(i).getVolume(), sortedList.get(i).getPrice()));
+                safe.setVolume(safe.getVolume() - sumVolume);
+            } else {
+                int newNumber = safe.getVolume() / sortedList.get(i).getVolume();
+                if (newNumber > 0) {
+                    resultList.add(new Item(sortedList.get(i).getName(), newNumber,
+                            sortedList.get(i).getVolume(), sortedList.get(i).getPrice()));
+                    break;
+                }
+            }
+        }
+        return resultList;
+    }
+
+    public void printItemInTheSafeWithMaxPrice(List<Item> resultList) {
+        resultList.forEach(System.out::println);
+    }
+}
+
