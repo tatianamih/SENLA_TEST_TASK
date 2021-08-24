d3f67bf06fdafe90af061f04b42b7869444a8902
“tatianamih” <tmihalchenko66@gmail.com>
2021-08-24 13:24:26 +0300

    Task1 Adding the App class using the SOLID (Single responsibility prince) principle.

---
+++ senla_tasks/src/senla/mikhalchenko/task1/app/App.java
@@ -0,0 +1,10 @@
+package senla.mikhalchenko.task1.app;
+
+import senla.mikhalchenko.task1.view.ConsoleUI;
+
+public class App {
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.consoleInput();
+    }
+}
