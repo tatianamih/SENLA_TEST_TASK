ce6795e56ee8814a84b64eb839511cf9556e91dc
Dmitry Chueshov <33500848+ChueshovDmitry@users.noreply.github.com>
2021-08-24 10:55:42 +0300

    Update Task1.java
    разнеси на два класса. Это не хорошая практика в в классе который для входа  в программу писать еще и логику. Не по ООП, нарушен принцип SOLID (Single responsibility prince)

--- senla_tasks/src/senla/mikhalchenko/task1/Task1.java
+++ senla_tasks/src/senla/mikhalchenko/task1/Task1.java
@@ -7,30 +7,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task1 {
-
-    private void getRepeatedCharactersInARowXTimes(String text, int numberOfDuplicateElements) {
-        int count = 0;
-        Matcher matcher = Pattern.compile("(.)\\1+").matcher(text);
-        while (matcher.find()) {
-            String subString = matcher.group();
-            if (subString.length() == numberOfDuplicateElements) {
-                count++;
-            } else {
-                count += 0;
-            }
-        }
-        System.out.println("\n" + "The number of consecutive repeating " + numberOfDuplicateElements +
-                " identical elements in the string is equal to " + count);
-    }
-
+  
     public static void main(String[] args) {
         System.out.println("Input string");
         Task1 task1 = new Task1();
 
         Scanner input = new Scanner(System.in);
         String str = input.next();
-
+        Code code = new Code();
         System.out.println("You input string is: " + str);
-        task1.getRepeatedCharactersInARowXTimes(str, 3);
+        code.getRepeatedCharactersInARowXTimes(str, 3);
     }
 }
