519a9dbfad696611407ba1b9949ab6f0fdb3a3e1
“tatianamih” <tmihalchenko66@gmail.com>
2021-08-24 13:21:13 +0300

    Task1 Changing the logic using the SOLID (Single responsibility principle) principle.

---
+++ senla_tasks/src/senla/mikhalchenko/task1/service/Service.java
@@ -0,0 +1,24 @@
+package senla.mikhalchenko.task1.service;
+/*Напишите программу, которая возвращает количество "троек" в заданной строке.
+Тройка - это символ, появляющийся три раза подряд в строке.*/
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Service {
+    public void getRepeatedCharactersInARowXTimes(String text, int numberOfDuplicateElements) {
+        int count = 0;
+        Matcher matcher = Pattern.compile("(.)\\1+").matcher(text);
+        while (matcher.find()) {
+            String subString = matcher.group();
+            if (subString.length() == numberOfDuplicateElements) {
+                count++;
+            } else {
+                count += 0;
+            }
+        }
+        System.out.println("\n" + "The number of consecutive repeating " + numberOfDuplicateElements +
+                " identical elements in the string is equal to " + count);
+    }
+}
